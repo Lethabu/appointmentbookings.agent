dd03f4c8d6635a9c11a8ffbd45506515e185fa41
Lethabu <thabangt01@gmail.com>
2025-06-28 04:37:52 +0200

    chore: clean up unused code and optimize imports in app.js

--- dist/app.js
+++ dist/app.js
@@ -3,27 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
-const socket_io_1 = require("socket.io");
-const security_1 = require("./middleware/security");
-const reminder_1 = __importDefault(require("./routes/reminder"));
-const agent_1 = __importDefault(require("./routes/agent"));
-const calendar_1 = require("./services/calendar");
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(security_1.advancedSecurity);
-app.use('/reminder', reminder_1.default);
-app.use('/agent', agent_1.default);
-const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server, { cors: { origin: '*' } });
-(0, calendar_1.setSocketServer)(io);
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-});
-server.listen(process.env.PORT || 3000, () => {
-    console.log('Server running');
-});
-exports.default = app;
+const jsx_runtime_1 = require("react/jsx-runtime");
+const react_router_dom_1 = require("react-router-dom");
+const Sidebar_1 = __importDefault(require("./components/Sidebar"));
+const Header_1 = __importDefault(require("./components/Header"));
+const DashboardPage_1 = __importDefault(require("./components/DashboardPage"));
+const AgentChatPage_1 = __importDefault(require("./components/AgentChatPage"));
+const BookingsPage_1 = __importDefault(require("./components/BookingsPage"));
+const constants_1 = require("./constants");
+const App = () => {
+    return ((0, jsx_runtime_1.jsx)(react_router_dom_1.HashRouter, { children: (0, jsx_runtime_1.jsxs)("div", { className: "flex h-screen bg-neutral-100", children: [(0, jsx_runtime_1.jsx)(Sidebar_1.default, {}), (0, jsx_runtime_1.jsxs)("div", { className: "flex-1 flex flex-col overflow-hidden", children: [(0, jsx_runtime_1.jsx)(Header_1.default, {}), (0, jsx_runtime_1.jsx)("main", { className: "flex-1 overflow-x-hidden overflow-y-auto bg-neutral-100 p-6", children: (0, jsx_runtime_1.jsxs)(react_router_dom_1.Routes, { children: [(0, jsx_runtime_1.jsx)(react_router_dom_1.Route, { path: "/", element: (0, jsx_runtime_1.jsx)(react_router_dom_1.Navigate, { to: constants_1.AppRoutes.DASHBOARD, replace: true }) }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, { path: constants_1.AppRoutes.DASHBOARD, element: (0, jsx_runtime_1.jsx)(DashboardPage_1.default, {}) }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, { path: constants_1.AppRoutes.AGENT_CHAT, element: (0, jsx_runtime_1.jsx)(AgentChatPage_1.default, {}) }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, { path: constants_1.AppRoutes.BOOKINGS, element: (0, jsx_runtime_1.jsx)(BookingsPage_1.default, {}) })] }) })] })] }) }));
+};
+exports.default = App;
